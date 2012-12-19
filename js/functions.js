9c172f04262bae673e5333a125a0c81f8178bc94
Omar Carpinteyro <dracoryet@gmail.com>
2012-12-19 13:41:22 -0600

    Primer Versión
    Cmit: Primer versión

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+﻿$(function(){
+	
+	if (typeof $.fn.jCarouselLite == 'function') {
+		$("#firstCarrucelGalLeft").jCarouselLite({
+			btnNext: "#firstCarGalRight",
+			btnPrev: "#firstCarGalLeft",
+			visible: 1,
+			speed: 850				
+		});
+	}
+	
+	$(".deparments li:first-child").addClass("noMarginLeft");
+	$(".deparments li:last-child").addClass("noMarginRight");
+	$("nav ul li:first-child").addClass("firstChild");
+	
+});
+
+function autorun()
+{
+}
+if (window.addEventListener) window.addEventListener("load", autorun, false);
+else if (window.attachEvent) window.attachEvent("onload", autorun);
+else window.onload = autorun;
